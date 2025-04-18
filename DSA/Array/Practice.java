24cb8a97ff1b2f781543b2ccf3e04e2cbea32a26
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-04-18 08:19:59 +0530

    practice

--- DSA/Array/Practice.java
+++ DSA/Array/Practice.java
@@ -7,7 +7,7 @@ public class Practice {
         arr[2] = 10;
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = i++;
+            arr[i] = i + 1;
             System.out.print(arr[i] + "  ");
 
         }
