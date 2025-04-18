b176302437081f58c31c645a5eab9a179d6090b6
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-04-18 09:04:08 +0530

    Reversed array

--- DSA/Array/practice/ReverseCharArray.java
+++ DSA/Array/practice/ReverseCharArray.java
@@ -25,3 +25,46 @@ public class ReverseCharArray {
     }
 }
 // Output: Reversed array: olleh
+
+//for Integer array
+// public class ReverseIntegerArray {
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 4, 5};
+//         reverse(arr);
+//         System.out.println("Reversed array: " + Arrays.toString(arr));
+//     }
+//
+//     public static void reverse(int[] arr) {
+//         int start = 0;
+//         int end = arr.length - 1;
+//         while (start < end) {
+//             int temp = arr[start];
+//             arr[start] = arr[end];
+//             arr[end] = temp;
+//             start++;
+//             end--;
+//         }
+//     }
+// }
+// Output: Reversed array: [5, 4, 3, 2, 1]
+// for String array
+// public class ReverseStringArray {
+//     public static void main(String[] args) {
+//         String[] arr = {"hello", "world", "java"};
+//         reverse(arr);
+//         System.out.println("Reversed array: " + Arrays.toString(arr));
+//     }
+//
+//     public static void reverse(String[] arr) {
+//         int start = 0;
+//         int end = arr.length - 1;
+//         while (start < end) {            
+// String temp = arr[start];
+//             arr[start] = arr[end];
+//             arr[end] = temp;
+//             start++;
+//             end--;
+//         }
+//     }
+// }
+// Output: Reversed array: [java, world, hello]
