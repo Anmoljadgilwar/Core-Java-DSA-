08b0a9da6a8f29b18a1fcd964b36f29dd4245c2c
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-04-25 08:49:36 +0530

    Practice

--- DSA/Sorting/SelectionSort/Selection.java
+++ DSA/Sorting/SelectionSort/Selection.java
@@ -1,7 +1,4 @@
-
 import java.util.Arrays;
-
-
 public static void main(String[] args) {
    int[] arr = {4, 6, 2, 8, 10};
    selection(arr);
@@ -11,16 +8,21 @@ public static void main(String[] args) {
 static int selection (int[] arr){
 
    for (int i = 0; i < arr.length; i++){
+      //find the max element in the remaining array and swap it with the first element
        int last = arr.length - i - 1;
-   }
+       int maxIndex = getmax(arr, 0, last);
+       swap(arr, maxIndex, last);
 
+   }
+   return 0;
 }
+
 
 static int swap(int[] arr, int first, int second){
     int temp = arr[first];
     arr[first] = arr[second];
     arr[second] = temp;
-   //  return arr[first];
+    return arr[first];
 
 }
 
