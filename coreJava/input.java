931b5061e23797a705e335138a3c8c77cec1ca70
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-05-13 18:32:00 +0530

    Practice

---
+++ coreJava/input.java
@@ -0,0 +1,15 @@
+
+// import java.util.Scanner;
+// public class input {
+//     public static void main(String[] args) {
+//         Scanner objScanner = new Scanner(System.in);
+//         System.out.print("Enter a string: ");
+//         String input = objScanner.nextLine(); // Reads the entire line as a string
+//         System.out.println("You entered: " + input);
+//         objScanner.close(); // Close the scanner to prevent resource leaks
+//         //if I don't close the scanner, it will not work in the next line of code.
+//         // It will throw an error: java.util.NoSuchElementException: No line found
+//     }
+// }
+//
+//Multiple input
