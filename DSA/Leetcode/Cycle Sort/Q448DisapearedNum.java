99fa0b13a3eaa34b260a6bd544d8801854db634e
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-07-05 12:44:34 +0530

    Leetcode problem  287

--- DSA/Leetcode/Cycle Sort/Q448DisapearedNum.java
+++ DSA/Leetcode/Cycle Sort/Q448DisapearedNum.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 import java.util.List;
 // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 // Asked in Google
+// 448. Find All Numbers Disappeared in an Array
+// Easy
+// Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+//
+// Find all the elements of [1, n] inclusive that do not appear in this array.
 
 class FindAllMissing {
 
