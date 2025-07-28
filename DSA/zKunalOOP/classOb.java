04b98e76bb9e32edbb8c19a8012009403f5a1fde
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-07-28 06:17:13 +0530

    OOP in Depth

---
+++ DSA/zKunalOOP/classOb.java
@@ -0,0 +1,17 @@
+
+public class classOb {
+
+    public static void main(String[] args) {
+
+    }
+
+    //create a class
+    //for every single student
+    class Student {
+
+        int rollNo;
+        String name;
+        float marks;
+    }
+
+}
