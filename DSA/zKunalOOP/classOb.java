a909ce53f7cc254d768840e43edb29b1807d4e48
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-07-29 20:44:06 +0530

    OOP in depth

--- DSA/zKunalOOP/classOb.java
+++ DSA/zKunalOOP/classOb.java
@@ -3,6 +3,14 @@ public class classOb {
 
     public static void main(String[] args) {
 
+        s1 = new Student();
+        s1.rollNo = 1;
+        s1.name = "Kunal";
+        s1.marks = 96.1;
+        System.out.println(s1.rollNo);
+        System.out.println(s1.name);
+        System.out.println(s1.marks);
+
     }
 
     //create a class
