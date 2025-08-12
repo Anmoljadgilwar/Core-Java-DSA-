c58e8c0524a7b8819b2607bfb8d0e2f818eedfa2
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-08-12 21:03:53 +0530

    Daily DSA practice

--- DSA/StringBuilder/reverseStr.java
+++ DSA/StringBuilder/reverseStr.java
@@ -4,12 +4,10 @@ public class reverseStr {
     public static void main(String[] args) {
         StringBuilder str = new StringBuilder("Hari Bol");
         System.out.println(str.reverse());
-    }
-}
 
-
-/*
-    for (int i = str.length() - 1; i >= 0; i--) {
+        for (int i = str.length() - 1; i >= 0; i--) {
             System.out.print(str.charAt(i));
         }
- */
+    }
+
+}
