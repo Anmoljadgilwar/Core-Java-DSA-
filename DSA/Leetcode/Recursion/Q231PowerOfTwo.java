70019f5ca9026574d7707c0170362d63c4ba140b
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-09-04 22:23:53 +0530

    Added time & space complexity

--- DSA/Leetcode/Recursion/Q231PowerOfTwo.java
+++ DSA/Leetcode/Recursion/Q231PowerOfTwo.java
@@ -12,4 +12,27 @@ public class Q231PowerOfTwo {
         }
         return false;
     }
+
+    public static void main(String[] args) {
+        Q231PowerOfTwo obj = new Q231PowerOfTwo();
+        System.out.println(obj.isPowerOfTwo(16));
+
+    }
 }
+
+// execution logic Explaination:
+// 1. n = 16
+// 2. n % 2 == 0
+// 3. n / 2 = 8
+// 4. n = 8
+// 5. n % 2 == 0
+// 6. n / 2 = 4
+// 7. n = 4
+// 8. n % 2 == 0
+// 9. n / 2 = 2
+// 10. n = 2
+// 11. n % 2 == 0
+// 12. n / 2 = 1
+// 13. n = 1
+//space complexity O(1)
+//time complexity O(log n)
