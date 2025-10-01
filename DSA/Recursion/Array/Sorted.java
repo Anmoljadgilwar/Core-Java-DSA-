7c77105a2bbac8aa0339fede833b5bdf341e9e57
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-10-01 17:25:16 +0530

    add: Recursion in Array

---
+++ DSA/Recursion/Array/Sorted.java
@@ -0,0 +1,16 @@
+
+public class Sorted {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 4, 3, 4, 5};
+        System.out.println(sorted(arr, 0));
+    }
+
+    public static boolean sorted(int[] arr, int index) {
+        if (index == arr.length - 1) {
+            return true;
+        }
+        return arr[index] < arr[index + 1] && sorted(arr, index + 1);
+    }
+}
