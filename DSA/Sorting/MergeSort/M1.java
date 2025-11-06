86eebd64d4e34cd35e8413a36878ec793e11823a
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-11-06 07:59:57 +0530

    fit: input changed

--- DSA/Sorting/MergeSort/M1.java
+++ DSA/Sorting/MergeSort/M1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class M1 {
 
     public static void main(String[] args) {
-        int[] arr = {5, 4, 1, 3, 2};
+        int[] arr = {5, 4, 2, 1, 3, 2};
         arr = mergeSort(arr);
         System.out.println(Arrays.toString(arr));
     }
