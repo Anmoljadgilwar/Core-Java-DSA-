efbed59c438081d0497fa2761620239bcd377e19
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-12-03 20:27:37 +0530

    add: backtracking

---
+++ DSA/Backtracking/Backtrack1.java
@@ -0,0 +1,12 @@
+
+class Backtrack1 {
+
+    public static void main(String[] args) {
+        String str = "ABC";
+
+    }
+
+    public static void backtrack(String str, String perm, int index) {
+
+    }
+}
