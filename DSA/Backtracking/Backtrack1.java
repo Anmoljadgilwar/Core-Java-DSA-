9221c4f342c0d80751ccafd2d6e29c9995a2160e
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-12-04 19:43:01 +0530

    fit: backtracking comments added

--- DSA/Backtracking/Backtrack1.java
+++ DSA/Backtracking/Backtrack1.java
@@ -1,12 +1,33 @@
 
 class Backtrack1 {
 
+    public static void printPermulation(String str, String perm, int idx) {
+
+        if (str.length() == 0) {
+            System.out.println(perm);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            printPermulation(newStr, perm + currChar, idx + 1);
+        }
+
+    }
+
     public static void main(String[] args) {
         String str = "ABC";
-
+        printPermulation(str, "", 0);
     }
 
-    public static void backtrack(String str, String perm, int index) {
-
-    }
 }
+
+// output:
+// ABC
+// ACB
+// BAC
+// BCA
+// CAB
+// CBA
