e13d6958c05b0fb8e178a07fb681c26bb2a3f8b3
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-12-10 15:35:55 +0530

    add: permutation

---
+++ DSA/Backtracking/BacktrackingSubsets.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+public class BacktrackingSubsets {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        List<Integer> current = new ArrayList<>();
+        subsets(arr, 0, current);
+    }
+
+    static void subsets(int[] arr, int index, List<Integer> current) {
+        if (index == arr.length) {
+            System.out.println(current);
+            return;
+        }
+
+        // 1️⃣ INCLUDE step
+        current.add(arr[index]);
+        subsets(arr, index + 1, current);
+
+        // 2️⃣ UNDO step (BACKTRACK)
+        current.remove(current.size() - 1);
+
+        // 3️⃣ EXCLUDE step
+        subsets(arr, index + 1, current);
+    }
+}
---
+++ DSA/Backtracking/Permutation/permutation.md
@@ -0,0 +1,8 @@
+### **Permutation (in short)**
+
+A **permutation** is an arrangement or ordering of all elements of a set.
+In DSA, permutation means generating or checking all possible orderings of elements in a sequence.
+
+**Example:**
+For array `[1, 2, 3]`, possible permutations are:
+`[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]`.
