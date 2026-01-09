0392a2b6ea0bc366aa73d81f20edb7ecc197fefb
Anmol Jadgilwar <developer.anmol108@gmail.com>
2026-01-09 20:07:38 +0530

    improve: comments added

--- DSA/Sorting/BubbleSorting/Selection.java
+++ DSA/Sorting/BubbleSorting/Selection.java
@@ -1,4 +1,39 @@
-import java.util.Arrays;
+// •	Finds the smallest element and swaps it with the first unsorted element.
+// •	Repeats for the rest of the array.
+
+class Selection {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Find the index of the minimum element in the unsorted part
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum with the element at i
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
 //
 //class Selection{
 //    public static void main(String[] args) {
@@ -41,38 +76,3 @@ import java.util.Arrays;
 //    }
 //
 //}
-//
-//
-class Selection {
-    
-        public static void selectionSort(int[] arr) {
-            int n = arr.length;
-
-            for (int i = 0; i < n - 1; i++) {
-                int minIndex = i;
-
-                // Find the index of the minimum element in the unsorted part
-                for (int j = i + 1; j < n; j++) {
-                    if (arr[j] < arr[minIndex]) {
-                        minIndex = j;
-                    }
-                }
-
-                // Swap the found minimum with the element at i
-                int temp = arr[minIndex];
-                arr[minIndex] = arr[i];
-                arr[i] = temp;
-            }
-        }
-
-        public static void main(String[] args) {
-            int[] arr = {64, 25, 12, 22, 11};
-
-            selectionSort(arr);
-
-            System.out.println("Sorted array:");
-            for (int num : arr) {
-                System.out.print(num + " ");
-            }
-        }
-    }
--- DSA/Sorting/BubbleSorting/insertionS.java
+++ DSA/Sorting/BubbleSorting/insertionS.java
@@ -11,7 +11,7 @@ public class insertionS {
 
     static void insertion(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j > 0; j--) {
+            for (int j = i + 1; j > 0; j--) {  //logic yaha hai i+1, j--
                 if (arr[j] < arr[j - 1]) {
                     swap(arr, j, j - 1);
                 } else {
