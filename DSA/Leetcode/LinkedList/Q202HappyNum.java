1ba3ff8104983937c0faf1b34ecfeaa843020840
Anmol Jadgilwar <developer.anmol108@gmail.com>
2026-04-06 17:53:30 +0530

    Leetcode 202.

---
+++ DSA/Leetcode/LinkedList/Q202HappyNum.java
@@ -0,0 +1,45 @@
+// 202. Happy Number
+// https://leetcode.com/problems/happy-number/
+// Easy
+// Given an integer n, return true if and only if it is a happy number.
+// A happy number is a number defined by the following process:
+// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+// Those numbers for which this process ends in 1 are happy.
+// Return true if n is a happy number, and false if not.
+// Example 1:
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+// Example 2:
+// Input: n = 2
+// Output: false
+
+class Solution {
+
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        }
+
+        return fast == 1;
+    }
+
+    private int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
